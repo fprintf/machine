ffff6b0622bcf06eae4dbd7a5c97129827759b2d
fprintf <fprintf@git.com>
2015-06-23 00:55:42 -0400

    Add more personality to the tell command

--- modules/tell.pm
+++ modules/tell.pm
@@ -37,12 +37,27 @@ sub tell {
 
     my ($to, $message) = split(/\s/, $arg, 2);
     my $ident = lc($to);
+    my $from = $irc->nick();
 
     # Add message to the queue
     $messages{$ident} ||= [];
     push(@{$messages{$to}}, {'time' => time, from => $irc->nick, message => $message});
 
-    $irc->say("I'll be sure to let $to know when they're around.");
+
+    my @responses = (
+        "$from: I'll be sure to let $to know when they're arround.",
+        "$from: Ok, ok, stop bothering me!",
+        "$from: You got it, friend.",
+        "$from: Yeah, yeah, I'll let them know -- you sure talk to $to a lot. You gotta thing for them or something?",
+        "$from: Right right, my brother.",
+        "$from: Will do.",
+        "$from: As you wish.",
+        "$from: $to will be notified as soon as possible.",
+        "$from: Ok boss.",
+        "$from: Ok cheif.",
+        "$from: Gotcha."
+    );
+    $irc->say($responses[int(rand(scalar(@responses)))]);
 }
 
 
