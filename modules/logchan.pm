70cbb9e6047f6388f3f915812267b4041978baa6
fprintf <fprintf@devnull.xo>
2015-12-12 22:09:59 -0800

    Update logchan to use shared variable -- there are some bugs though
    We need to add the ability to turn OFF the logging (remove the binding)
    and also something is now causing the children to segfault on startup
    possibly since adding this module..?

--- modules/logchan.pm
+++ modules/logchan.pm
@@ -4,10 +4,15 @@ use warnings;
 
 use mod_perl::commands;
 
+use IPC::Shareable;
+
 # Table of sources and destinations
 # source => [ 'destination', 'destination', .... ]
 my %logchan_routes;
 
+tie %logchan_routes, 'IPC::Shareable', 'logchan', {
+    exclusive => 0, destroy => 1, create => 1
+};
 
 # Handler to listen for routable messages and route them as appropriate
 mod_perl::commands::register_handler('PRIVMSG', \&logchan_handler);
@@ -18,11 +23,12 @@ sub logchan_handler {
     my ($irc) = @_;
 
 	my $source = $irc->servername . ":" . $irc->target;
-	my $entries = $logchan_routes($source);
+	my $entries = $logchan_routes{$source};
 	if ($entries && @$entries) {
 		foreach my $dest (@$entries) {
 			my ($server, $target) = split(/:/, $dest, 2);
-			$irc->privmsg_server($server, $target, $irc->text);
+#			print STDERR "sending message to $server $target :".$irc->text."\n";
+			$irc->privmsg_server($server, $target, "[$source] <".$irc->nick."> " . $irc->text);
 		}
 	}
 }
@@ -39,7 +45,7 @@ sub logchan {
 	}
 
 	$source = lc($source);
-	$source = lc($dest);
+	$dest = lc($dest);
 
 	# Prepend the servername if it wasn't given
 	if ($dest !~ /:/) {
@@ -50,10 +56,11 @@ sub logchan {
 	}
 
 	# Add the entry if its not there already
-	if (exists($logchan_routes{$source})) {
-		my $entries = $logchan_routes{$source};
-		push(@{$logchan_routes{$source}}, $dest) if (!grep($dest, @$entries));
+	if (!exists($logchan_routes{$source})) {
+		$logchan_routes{$source} = [];
 	}
+	my $entries = $logchan_routes{$source};
+	push(@{$logchan_routes{$source}}, $dest) if (!grep($dest, @$entries));
 
 	$irc->say("Added destination $dest for source $source");
 }
