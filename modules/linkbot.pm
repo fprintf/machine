8e7069b73a236a24e28f120927954a6fa1dc4061
fprintf <fprintf@github.com>
2018-06-16 15:35:52 -0400

    Update UserAgent and add common default_headers to seem like a normal browser

--- modules/linkbot.pm
+++ modules/linkbot.pm
@@ -45,8 +45,12 @@ sub get_user_agent
 		'timeout' => 120,
 		'max_redirect' => 8,
 		'max_size' => 384 * 1024 * 1024,  # 384K
-		'agent' => 'Mozilla/5.0 (X11; Linux i686; rv:21.0) Gecko/20100101 Firefox/21.0'
+		'agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 	);
+
+	$ua->default_header('Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8');
+	$ua->default_header('Accept-Encoding' => 'gzip, deflate, br');
+	$ua->default_header('Accept-Language' => 'en-US,en;q=0.5');
 
     return $ua;
 }
